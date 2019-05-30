a9c1115d75227c2dabe810f0c96cbb66a46056cd
thinkverse <ctuvedsson@gmail.com>
2019-05-30 20:19:04 +0200

    update: Added update url

--- src/main/java/dev/thinkverse/verticalslabsmod/VerticalSlabsMod.java
+++ src/main/java/dev/thinkverse/verticalslabsmod/VerticalSlabsMod.java
@@ -17,7 +17,7 @@ import java.util.Random;
 public class VerticalSlabsMod
 {
     public static final String MODID = "verticalslabsmod";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
 
     public static VerticalSlabsMod INSTANCE;
     public static Proxy PROXY;
