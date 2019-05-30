b98dbe8bbfe1a77f07b6195bb7351e8044ce60dd
thinkverse <ctuvedsson@gmail.com>
2019-05-30 19:10:00 +0200

    update: Vertical Slabs now connect

---
+++ src/main/java/dev/thinkverse/verticalslabsmod/blocks/shapes/VerticalSlabShape.java
@@ -0,0 +1,25 @@
+package dev.thinkverse.verticalslabsmod.blocks.shapes;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum VerticalSlabShape implements IStringSerializable {
+    STRAIGHT("straight"),
+    INNER_LEFT("inner_left"),
+    INNER_RIGHT("inner_right"),
+    OUTER_LEFT("outer_left"),
+    OUTER_RIGHT("outer_right");
+
+    private final String name;
+
+    private VerticalSlabShape(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
