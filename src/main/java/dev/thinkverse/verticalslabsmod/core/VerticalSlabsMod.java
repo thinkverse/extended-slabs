018b83a7136e24ecdc077570f01db22eb49820b2
thinkverse <ctuvedsson@gmail.com>
2019-06-08 02:25:48 +0200

    update: beta versioning

--- src/main/java/dev/thinkverse/verticalslabsmod/core/VerticalSlabsMod.java
+++ src/main/java/dev/thinkverse/verticalslabsmod/core/VerticalSlabsMod.java
@@ -19,7 +19,7 @@ import java.util.Random;
 public class VerticalSlabsMod
 {
     public static final String MODID = "verticalslabsmod";
-    public static final String VERSION = "0.3.3";
+    public static final String VERSION = "0.4.0-beta";
 
     public static VerticalSlabsMod INSTANCE;
 
