9bca0017315c2cd965bef5297d177df14d392d24
thinkverse <ctuvedsson@gmail.com>
2019-06-22 16:25:47 +0200

    change: vertical slab interfaces

--- src/main/java/dev/thinkverse/extendedslabs/blocks/BlockVerticalSlab.java
+++ src/main/java/dev/thinkverse/extendedslabs/blocks/BlockVerticalSlab.java
@@ -13,7 +13,6 @@ import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -23,7 +22,7 @@ import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
 
-public class BlockVerticalSlab extends Block implements IBucketPickupHandler, ILiquidContainer {
+public class BlockVerticalSlab extends Block implements IWaterLoggable {
     public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final EnumProperty<VerticalSlabShape> SHAPE = EnumProperty.create("shape", VerticalSlabShape.class);
@@ -46,12 +45,6 @@ public class BlockVerticalSlab extends Block implements IBucketPickupHandler, IL
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(FACING, SHAPE, WATERLOGGED);
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public boolean func_220074_n(BlockState state) {
-        return false;
     }
 
     @Override
@@ -221,16 +214,7 @@ public class BlockVerticalSlab extends Block implements IBucketPickupHandler, IL
         }
 
         /* First round of updatePostPlacement */
-        return facing.getAxis().isHorizontal() ? stateIn.with(SHAPE, getSlabShape(stateIn, worldIn, currentPos)) : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);    }
-
-    @Override
-    public Fluid pickupFluid(IWorld worldIn, BlockPos pos, BlockState state) {
-        if (state.get(WATERLOGGED)) {
-            worldIn.setBlockState(pos, state.with(WATERLOGGED, Boolean.valueOf(false)), 3);
-            return Fluids.WATER;
-        } else {
-            return Fluids.EMPTY;
-        }
+        return facing.getAxis().isHorizontal() ? stateIn.with(SHAPE, getSlabShape(stateIn, worldIn, currentPos)) : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
     }
 
     @Override
@@ -241,21 +225,12 @@ public class BlockVerticalSlab extends Block implements IBucketPickupHandler, IL
 
     @Override
     public boolean canContainFluid(IBlockReader worldIn, BlockPos pos, BlockState state, Fluid fluidIn) {
-        return !state.get(WATERLOGGED) && fluidIn == Fluids.WATER;
+        return IWaterLoggable.super.canContainFluid(worldIn, pos, state, fluidIn);
     }
 
     @Override
     public boolean receiveFluid(IWorld worldIn, BlockPos pos, BlockState state, IFluidState fluidStateIn) {
-        if (!state.get(WATERLOGGED) && fluidStateIn.getFluid() == Fluids.WATER) {
-            if (!worldIn.isRemote()) {
-                worldIn.setBlockState(pos, state.with(WATERLOGGED, Boolean.valueOf(true)), 3);
-                worldIn.getPendingFluidTicks().scheduleTick(pos, fluidStateIn.getFluid(), fluidStateIn.getFluid().getTickRate(worldIn));
-            }
-
-            return true;
-        } else {
-            return false;
-        }
+        return IWaterLoggable.super.receiveFluid(worldIn, pos, state, fluidStateIn);
     }
 
 }
